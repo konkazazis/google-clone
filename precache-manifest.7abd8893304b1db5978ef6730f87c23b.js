82665f18aafbeafe88d2644425fbbe7e3cec3492
Konstantinos <nikos_kazazis@yahoo.com>
2020-09-18 12:57:54 +0300

    Updates

---
+++ precache-manifest.7abd8893304b1db5978ef6730f87c23b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a40b165cebff9fb449ac78222a4328a",
+    "url": "/google-clone/index.html"
+  },
+  {
+    "revision": "e92614e2a008651183e2",
+    "url": "/google-clone/static/css/main.b46baf85.chunk.css"
+  },
+  {
+    "revision": "e13a430e2a83f5943b41",
+    "url": "/google-clone/static/js/2.a8df02e5.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/google-clone/static/js/2.a8df02e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e92614e2a008651183e2",
+    "url": "/google-clone/static/js/main.de6e2450.chunk.js"
+  },
+  {
+    "revision": "53e6f9d7fcbadc32d4de",
+    "url": "/google-clone/static/js/runtime-main.20aa3e5d.js"
+  }
+]);
